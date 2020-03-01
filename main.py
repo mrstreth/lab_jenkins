c81bb0732e726bfd12a65a711da7b6deec798604
mrstreth <mr.tesanta@gmail.com>
2020-03-01 16:25:43 +0500

    version 0.3 add classes_error

--- main.py
+++ main.py
@@ -1,45 +1,22 @@
 import sys
 import os.path
 import peewee
+import classes_error as Error
 
-version = 0.2
-
-class BaseError(Exception):
-    """Определение собственных классов ошибок"""
-    
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
-    
-class ArgsInputError(BaseError):
-    """Неправильный ввод аргументов программы"""
-    pass
-
-class TableNameError(BaseError):
-    """Не существует такой таблицы в БД"""
-    pass
-
-class NotFoundDocFile(BaseError):
-    """Файл doc.txt не найден"""
-    pass
-
-class OpenDocFileError(BaseError):
-    """Не удалось открыть doc.txt"""
-    pass
+version = 0.3
 
 if __name__ == '__main__':
 
     if len(sys.argv) == 1 or sys.argv[1] == '--help':
         if not os.path.exists('doc.txt'):
-            raise NotFoundDocFile('Файл doc.txt не найден')
+            raise Error.NotFoundDocFile('Файл doc.txt не найден')
         try:
             file_doc = open('doc.txt')
             for line in file_doc:
                 print(line)
             file_doc.close()
         except:
-            raise OpenDocFileError('Не удалось открыть doc.txt')
+            raise Error.OpenDocFileError('Не удалось открыть doc.txt')
 
     elif len(sys.argv) == 2 and sys.argv[1] == '--version' or sys.argv[1] == '-v':
         print(f'{version}')
@@ -55,7 +32,7 @@ if __name__ == '__main__':
         if sys.argv[2].upper() in A:
             print('показать')
         else:
-            raise TableNameError('Не существует такой таблицы в БД')
+            raise Error.TableNameError('Не существует такой таблицы в БД')
 
     else:
-        raise ArgsInputError('Неправильный ввод аргументов программы')
+        raise Error.ArgsInputError('Неправильный ввод аргументов программы')
