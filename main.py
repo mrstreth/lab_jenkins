ec23ab9da6b6757db41cdc95a691dcaeee1b38e1
mrstreth <mr.tesanta@gmail.com>
2020-03-01 18:15:01 +0500

    version 0.4 add init func

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
 import os.path
-import peewee
+from peewee import *
 import classes_error as Error
+import datetime
 
-version = 0.3
+version = 0.4
+name_databade = 'database.db'
 
 if __name__ == '__main__':
 
     if len(sys.argv) == 1 or sys.argv[1] == '--help':
+        """Вызов справки"""
         if not os.path.exists('doc.txt'):
             raise Error.NotFoundDocFile('Файл doc.txt не найден')
         try:
@@ -19,10 +22,48 @@ if __name__ == '__main__':
             raise Error.OpenDocFileError('Не удалось открыть doc.txt')
 
     elif len(sys.argv) == 2 and sys.argv[1] == '--version' or sys.argv[1] == '-v':
+        """Показать версию программы"""
         print(f'{version}')
         
     elif len(sys.argv) == 2 and sys.argv[1] == 'init':
-        print('create DB')
+        """Инициализация БД (создание пустой)"""
+        if os.path.exists(name_databade):
+            os.remove(name_databade)
+            
+        db = SqliteDatabase(name_databade)
+        
+        class BaseModel(Model):
+            class Meta:
+                database = db
+                
+        class CLIENTS(BaseModel):
+            """Таблица CLIENTS"""
+            NAME = CharField(max_length=50, unique=True, primary_key=True)
+            CITY = CharField(max_length=50)
+            ADDRESS = CharField(max_length=50)
+            
+            class Meta:
+                db_table = 'CLIENTS'
+                
+        class ODDERS(BaseModel):
+            """Таблица ODDERS"""
+            CLIENT = ForeignKeyField(CLIENTS, field='NAME')
+            DATE = DateTimeField(default=datetime.datetime.now())
+            AMOUNT = IntegerField()
+            DESCRIPTION = CharField(max_length=100)
+            
+        class Meta:
+            db_table = 'ODDERS'
+
+        try:
+            db.connect()
+            CLIENTS.create_table()
+            ODDERS.create_table()
+        except peewee.InternalError as px:
+            print(str(px))
+        
+            
+            
 
     elif len(sys.argv) == 2 and sys.argv[1] == 'fill':
         print('fill DB')
