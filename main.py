87e51b70001a15646190606e181adbcadc3b3e43
mrstreth <mr.tesanta@gmail.com>
2020-03-01 19:47:32 +0500

    version 0.6 add fill func

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import sys
 import os.path
 from peewee import *
+import peewee
 import classes_error as Error
 import datetime
+import random as rnd
 
-version = 0.4
-name_databade = 'database.db'
+version = 0.6
+name_database = 'database.db'
+
+db = SqliteDatabase(name_database)
+        
+class BaseModel(Model):
+    class Meta:
+        database = db
+                
+class CLIENTS(BaseModel):
+    """Таблица CLIENTS"""
+    ID = PrimaryKeyField()
+    NAME = CharField(max_length=50)
+    CITY = CharField(max_length=50)
+    ADDRESS = CharField(max_length=50)
+    class Meta:
+        db_table = 'clients'
+                
+class ODDERS(BaseModel):
+    """Таблица ODDERS"""
+    ID = PrimaryKeyField()
+    CLIENT = ForeignKeyField(CLIENTS)
+    DATE = DateTimeField(default=datetime.datetime.now())
+    AMOUNT = IntegerField()
+    DESCRIPTION = CharField(max_length=100)    
+    class Meta:
+        db_table = 'odders'
 
 if __name__ == '__main__':
 
@@ -27,34 +54,9 @@ if __name__ == '__main__':
         
     elif len(sys.argv) == 2 and sys.argv[1] == 'init':
         """Инициализация БД (создание пустой)"""
-        if os.path.exists(name_databade):
-            os.remove(name_databade)
-            
-        db = SqliteDatabase(name_databade)
+        if os.path.exists(name_database):
+            os.remove(name_database)
         
-        class BaseModel(Model):
-            class Meta:
-                database = db
-                
-        class CLIENTS(BaseModel):
-            """Таблица CLIENTS"""
-            NAME = CharField(max_length=50, unique=True, primary_key=True)
-            CITY = CharField(max_length=50)
-            ADDRESS = CharField(max_length=50)
-            
-            class Meta:
-                db_table = 'CLIENTS'
-                
-        class ODDERS(BaseModel):
-            """Таблица ODDERS"""
-            CLIENT = ForeignKeyField(CLIENTS, field='NAME')
-            DATE = DateTimeField(default=datetime.datetime.now())
-            AMOUNT = IntegerField()
-            DESCRIPTION = CharField(max_length=100)
-            
-        class Meta:
-            db_table = 'ODDERS'
-
         try:
             db.connect()
             CLIENTS.create_table()
@@ -62,12 +64,16 @@ if __name__ == '__main__':
         except peewee.InternalError as px:
             print(str(px))
         
-            
-            
-
     elif len(sys.argv) == 2 and sys.argv[1] == 'fill':
-        print('fill DB')
-
+        """Заполнение случайными записями (10)"""
+        try:
+            db.connect()
+            for i in range(10):
+                CLIENTS.create(NAME=f'NAME{i+1}',CITY=f'CITY{i+1}',ADDRESS=f'ADDRESS{i+1}')
+                ODDERS.create(CLIENT=rnd.randint(0,10),AMOUNT=i+1,DESCRIPTION=f'test')
+        except peewee.InternalError as px:
+            print(str(px))
+            
     elif len(sys.argv) == 3 and sys.argv[1] == 'show':
         A = ['CLIENTS','ODDERS']
         if sys.argv[2].upper() in A:
