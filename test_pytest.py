638efb809b3a9df7d27c3089014bfcbbffff8505
mrstreth <mr.tesanta@gmail.com>
2020-03-02 02:30:24 +0500

    change language in test_pytest.py

--- test_pytest.py
+++ test_pytest.py
@@ -3,26 +3,26 @@ import main
 import os.path
 
 def test_exists_database():
-    """Существует ли база даных"""
+    """Is there a database"""
     assert os.path.exists(main.name_database) == True
 
 def test_row_count_clients():
-    """Кол-во заполненных строк в clients = 10"""
+    """Number of Filled Rows in clients = 10"""
     assert len(main.CLIENTS.select()) == 10
 
 def test_row_count_odders():
-    """Кол-во заполненных строк в odders = 10"""
+    """Number of Filled Rows in odders = 10"""
     assert len(main.ODDERS.select()) == 10
 
 def test_coll_clients():
-    """Содерзит ли clients необходимые колонки"""
+    """Does clients contain necessary columns"""
     try:
         x = main.CLIENTS.select(main.CLIENTS.ID,main.CLIENTS.NAME,main.CLIENTS.CITY, main.CLIENTS.ADDRESS)
     except:
         assert False
 
 def test_coll_odders():
-    """Содерзит ли odders необходимые колонки"""
+    """Does odders contain necessary columns"""
     try:
         x = main.ODDERS.select(main.ODDERS.ID,main.ODDERS.CLIENT_id,main.ODDERS.DATE, main.ODDERS.AMOUNT,main.ODDERS.DESCRIPTION)
     except:
